4cb17b5d2a738cef244f46274cce48b3a22ae314
wangxpfly <18778922@qq.com>
2022-02-11 17:46:33 +0800

    commit

--- mytest/datatype.go
+++ mytest/datatype.go
@@ -6,14 +6,18 @@ import (
 )
 
 func Test_map() {
-	data := make([]interface{}, 2)
+	//data := make([]interface{}, 2)  固定数组
+	// 动态数组
+	var data []interface{}
+	//append(data,)
 	//var data [2]interface{}
-	for i := 0; i < 2; i++ {
+	for i := 0; i < 3; i++ {
 
 		datamap := make(map[string]interface{})
 		datamap["name"] = "ddd"
 		datamap["age"] = 20 + i
-		data[i] = datamap
+		//data[i] = datamap
+		data = append(data, datamap)
 	}
 
 	tmparr, _ := exdatatype.ToArray(data)
